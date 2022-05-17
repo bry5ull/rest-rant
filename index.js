8601287bae89a5cfe16b1311d691ce9930966aa5
bry5ull <bry@bry5ull.dev>
2022-05-17 17:09:22 -0500

    added home page view

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Rest-Rant')
+    res.render('home')
   })
 
 app.get('*', (req, res) => {
