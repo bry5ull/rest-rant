41be95e5cf793d11ae8701413f92724be9aad21e
bry5ull <bry@bry5ull.dev>
2022-05-19 16:57:27 -0500

    part 5

--- controllers/places.js
+++ controllers/places.js
@@ -1,31 +1,46 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: `Paulie's Pizza`,
-        city: 'Chicago',
-        state: 'IL',
-        cuisines: `Buttercrust Pizza, that's it`,
-        pic: '/images/pizza.jpg'
-      }, {
-        name: 'Sodium Expensive',
-        city: 'Austin',
-        state: 'TX',
-        cuisines: 'Luxury Salted Meats, Pickled Hipster Garnishes',
-        pic: '/images/salt.jpg'
-      }]
-      
-    res.render('places/index', {places})
+    res.render('places/index', { places })
 })
+
+// router.get('/', (req, res) => {
+//     let places = [{
+//         name: `Paulie's Pizza`,
+//         city: 'Chicago',
+//         state: 'IL',
+//         cuisines: `Buttercrust Pizza, that's it`,
+//         pic: '/images/pizza.jpg'
+//       }, {
+//         name: 'Sodium Expensive',
+//         city: 'Austin',
+//         state: 'TX',
+//         cuisines: 'Luxury Salted Meats, Pickled Hipster Garnishes',
+//         pic: '/images/salt.jpg'
+//       }]
+      
+//     res.render('places/index', {places})
+// })
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 
 module.exports = router
